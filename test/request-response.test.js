0d441e6c9e402ff284025c785cbf237b3e194f08
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-06-02 00:09:12 +0700

    Unit Test Response

---
+++ test/request-response.test.js
@@ -0,0 +1,18 @@
+import express from "express";
+import request from "supertest";
+
+
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("Hello World")
+});
+
+test("Request Url Unit Test", async ()=>{
+
+    const response = await request(app)
+    .get("/");
+    console.log(`${response.text}`)
+    expect(response.text).toBe("Hello World");
+});
