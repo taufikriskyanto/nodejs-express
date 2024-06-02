de878fb0c8a92457ad81a3461b0ce3804bd13c7e
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-06-02 21:28:33 +0700

    upload file

--- test/request-body.test.js
+++ test/request-body.test.js
@@ -1,46 +1,61 @@
-import express, { query, response } from "express";
+import express from "express";
 import request from "supertest";
-
-
+import expressFileUpload from "express-fileupload";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended: false}));
+app.use(expressFileUpload());
 
-app.get('/json', (request, response)=>{
-    const name = request.body.name;
-    response.json({
+app.post('/json', (req, res) => {
+    const name = req.body.name;
+    res.json({
         hello: `Hello ${name}`
-    })
-})
-
-app.post('/form', (request, response)=>{
-    const name = request.body.name;
-    response.json({
-        hello: `Hello ${name}`
-    })
-}) 
-test("Request Body Json Unit Test", async ()=>{
-
-    let response = await request(app)
-    .get("/json")
-    .set("Content-type", "application/json")
-    .send({name: "World"})
-    expect(response.body).toEqual({
-        hello: "Hello World"
     });
-
 });
 
-
-test("Request Body Form", async ()=>{
-
-    const response = await request(app)
-    .post("/form")
-    .set("Content-Type", "application/x-www-form-urlencoded")
-    .send("name=World");
-    expect(response.body).toEqual({
-        hello: "Hello World"
+app.post('/form', (req, res) => {
+    const name = req.body.name;
+    res.json({
+        hello: `Hello ${name}`
     });
+});
 
+app.post("/file", async (req, res) => {
+    const textFile = req.files.article;
+    await textFile.mv(__dirname + "/upload/" + textFile.name);
+
+    res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`);
+});
+
+test("Test Request File Upload", async () => {
+    const response = await request(app)
+        .post("/file")
+        .set("Content-Type", "multipart/form-data")
+        .field("name", "Eko")
+        .attach("article", __dirname + "/static/contoh.txt");
+
+    expect(response.text).toBe("Hello Eko, you uploaded contoh.txt");
+});
+
+test("Test Request JSON", async () => {
+    const response = await request(app)
+        .post("/json")
+        .set("Content-Type", "application/json")
+        .send({name: "World"});
+
+    expect(response.body).toEqual({
+        hello: `Hello World`
+    });
+});
+
+test("Test Request Form", async () => {
+    const response = await request(app)
+        .post("/form")
+        .set("Content-Type", "application/x-www-form-urlencoded")
+        .send("name=World");
+
+    expect(response.body).toEqual({
+        hello: `Hello World`
+    });
 });
